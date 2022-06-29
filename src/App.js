b06eee2f70cffba8489c9cb1ab963e30331c0d95
TimuXDXD <timu890518@gmail.com>
2022-06-29 18:03:19 +0800

    Refresh all

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-state = {
+class App extends Component {
+  state = {
     data: null
   };
 
   componentDidMount() {
     this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
+    .then(res => this.setState({ data: res.express }))
+    .catch(err => console.log(err));
   }
-    // fetching the GET route from the Express server which matches the GET route from server.js
+  // fetching the GET route from the Express server which matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
+    const response = await fetch('/backend');
+    // console.log(response);
     const body = await response.json();
-
+    // const body = response;
+    console.log(body);
     if (response.status !== 200) {
       throw Error(body.message)
     }
     return body;
   };
-
   render() {
     return (
       <div className="App">
