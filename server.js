992009c5075156338ee16e4dc8e6846697a0294c
TimuXDXD <timu890518@gmail.com>
2022-06-30 16:51:00 +0800

    Deploy test

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const favicon = require("serve-favicon");
 
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
 
 // API Routes here
