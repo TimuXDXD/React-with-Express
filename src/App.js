85530276de7930a3ab4c8f90ccc8d454b5ae9090
TimuXDXD <timu890518@gmail.com>
2022-06-30 15:33:54 +0800

    Deploy test

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ state = {
     data: null
   };
 
-  // componentDidMount() {
-  //   this.callBackendAPI()
-  //     .then(res => this.setState({ data: res.express }))
-  //     .catch(err => console.log(err));
-  // }
-  //   // fetching the GET route from the Express server which matches the GET route from server.js
-  // callBackendAPI = async () => {
-  //   const response = await fetch('/backend');
-  //   const body = await response.json();
-  //
-  //   if (response.status !== 200) {
-  //     throw Error(body.message)
-  //   }
-  //   return body;
-  // };
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // fetching the GET route from the Express server which matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
 
   render() {
     return (
