b41ddaaa272ec2cf66fb8cd1d61f0afcbe6bea0d
TimuXDXD <timu890518@gmail.com>
2022-06-30 12:19:56 +0800

    Refresh all again

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+
+// API Routes here
+
+
+// catchall
+app.get('/backend', (req, res) => {
+  res.send({express: 'Connected.'});
+});
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
